3e008e5077e93ea18209957cef9e90ec84206db5
Asif ghouri <asifarifghouri@hotmail.com>
2019-01-04 02:28:42 +0500

    testcommit

--- WebApplication3/Startup.cs
+++ WebApplication3/Startup.cs
@@ -16,7 +16,7 @@ using Microsoft.Extensions.DependencyInjection;
 namespace WebApplication3
 {
     public class Startup
-    {
+    {//checking github
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
